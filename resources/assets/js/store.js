345abb5a89d046af9a4b4af6648a83560a877176
Ekin Aslan <ekinaslan.js@gmail.com>
2024-08-10 18:59:08 +0300

    Fix

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -6,8 +6,8 @@ async function renderMusics() {
   list.innerHTML = "";
 
   musics.forEach((music) => {
-    list.innerHTML += `<li><a href="#"><img src="../resources/musics/images/${music.image}" /></a><div><span>${music.title}</span><span>${music.album}</span></div>
-  <audio class="soundwave" controls><source src="../resources/musics/audios/${music.audio}" type="audio/mp3" /></audio>
+    list.innerHTML += `<li><a href="#"><img src="https://www.echorbitaudio.com/resources/musics/images/${music.image}" /></a><div><span>${music.name}</span><span>${music.album}</span></div>
+  <audio class="soundwave" controls><source src="https://www.echorbitaudio.com/resources/musics/audios/${music.audio}" type="audio/mp3" /></audio>
   <div class="price" onclick="cart(${music.id})"><span>${music.price}$</span> <i class="fa-solid fa-cart-shopping"></i> Add to cart</div>
   <div class="like" onclick="like(${music.id})"><i class="fa-regular fa-heart"></i></div></li>`;
   });
