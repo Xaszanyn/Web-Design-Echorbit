81ca1b91c23318cde340771a3cd6be72787ae21a
Ekin Aslan <ekinaslan.js@gmail.com>
2024-10-21 22:04:52 +0300

    Showcase

--- resources/assets/js/home.js
+++ resources/assets/js/home.js
@@ -7,12 +7,8 @@ const showcase = document.querySelector("#showcase");
       (featuredShowcase.innerHTML += `<a href="/product?${product.id}"><img src="/resources/images/covers/${product.image}" /><span>${product.name}</span></a>`)
   );
 
-  showcases = await get("get.php?target=showcase");
-  // await loginUserSession();
-  // products = await get("get.php?target=products");
-  // allCategories = await get("get.php?target=categories");
-  // renderCartButton();
-  // renderProducts();
-  // renderFeatured();
-  // assign(cartButton, viewCart);
+  (await get("get.php?target=products")).forEach(
+    (product) =>
+      (showcase.innerHTML += `<a href="/product?${product.id}"><img src="/resources/images/covers/small/${product.image}" /></a>`)
+  );
 })();
