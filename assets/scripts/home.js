11e3bae6686382084bb9e3f49a3d7aa3dbdae99d
Ekin Aslan <ekinaslan.js@gmail.com>
2024-12-19 04:27:22 +0300

    Header Slider

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -2,6 +2,22 @@ const featuredShowcase = document.querySelector("#featured-showcase");
 const showcase = document.querySelector("#showcase");
 
 (async function () {
+  let slider = document.querySelector("header #slider");
+  let slides = slider.children.length;
+
+  document.querySelector("header i.right").addEventListener("click", () => {
+    let pointer = (parseInt(slider.dataset.slide) + 1) % slides;
+    slider.style.transform = `translateX(${pointer * -100}%)`;
+    slider.dataset.slide = pointer;
+  });
+
+  document.querySelector("header i.left").addEventListener("click", () => {
+    let pointer =
+      slider.dataset.slide != 0 ? slider.dataset.slide - 1 : slides - 1;
+    slider.style.transform = `translateX(${pointer * -100}%)`;
+    slider.dataset.slide = pointer;
+  });
+
   loginUserSession(false);
 
   (await get("get.php?target=featured-showcase")).forEach(
